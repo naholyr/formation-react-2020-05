f506268919675ff27a11fa387e520bc4a440e299
Nicolas Chambrier <naholyr@gmail.com>
2020-05-13 21:13:35 +0200

    Redux: dev tools

--- app/src/store.js
+++ app/src/store.js
@@ -1,11 +1,15 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { reducer, initialState } from './reducer';
 import { loginPersistence } from './redux-middlewares';
 import { init } from './actions';
 
-// Middlewares
-const storeEnhancer = applyMiddleware(loginPersistence);
+// Redux devtools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+// Middlewares
+const storeEnhancer = composeEnhancers(applyMiddleware(loginPersistence));
+
+// Store initialization
 export const store = createStore(reducer, initialState, storeEnhancer);
 
 // Initialization action
